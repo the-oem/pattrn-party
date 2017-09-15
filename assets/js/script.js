f915af1d0743ac4fa8ba3b8bd270e67f975e1303
Jason Collins <the.oem@gmail.com>
2017-09-15 08:44:52 -0600

    Update header bar.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 // Might need this later
+const menuBtn = document.querySelector('.menu-btn')
+
+menuBtn.addEventListener('click', () => {
+  console.log('clicked');
+  if(document.querySelector('.mobile-nav').style.display === 'inline'){
+    document.querySelector('.mobile-nav').style.display = 'none';
+  } else {
+    document.querySelector('.mobile-nav').style.display = 'inline';
+  }
+})
