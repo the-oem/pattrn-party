6b6ff132caff400e4a37b31d76bb243c24304043
Jason Collins <the.oem@gmail.com>
2017-09-15 11:18:14 -0600

    Remove erroneous console.log statement.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 const menuBtn = document.querySelector('.menu-btn')
 
 menuBtn.addEventListener('click', () => {
-  console.log('clicked');
   if(document.querySelector('.mobile-nav').style.display === 'inline'){
     document.querySelector('.mobile-nav').style.display = 'none';
   } else {
